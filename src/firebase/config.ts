9cd16efb5e076cc22c0e4cc6d825a072ea857cd7
Rafael Barros <rafael.nsb1995@gmail.com>
2025-11-01 00:29:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-860501466-71c83",
+  "appId": "1:355711685237:web:d428455fcdb1e99b141e3c",
+  "apiKey": "AIzaSyAJw1oySqSCS0oQIjvUK2BX224jd7F38p8",
+  "authDomain": "studio-860501466-71c83.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "355711685237"
+};
