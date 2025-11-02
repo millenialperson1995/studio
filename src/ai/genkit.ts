042a62aa77003be4983481325f02e1c450343b0e
Rafael Barros <rafael.nsb1995@gmail.com>
2025-11-02 20:29:50 +0000

    olha ainda estou com o problema, eu tenho que colocar a variavel do gemi

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,9 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiVersion: 'v1beta'})],
 });
