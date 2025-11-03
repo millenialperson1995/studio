5774aa881d7d61e6670fc90c5ea01a9bce520350
Rafael Barros <rafael.nsb1995@gmail.com>
2025-11-03 10:19:23 +0000

    Acho que tu mexeu na forma como o pwa funciona, porque depois que voce f

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import withPWAInit from '@ducanh2912/next-pwa';
 const withPWA = withPWAInit({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-  swMinify: false,
 });
 
 const nextConfig: NextConfig = {
